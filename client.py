e10ed0fe1cde9ea0f0ad4c7ea41d084814e11eb8
Francis <fozoka@turing.une.edu.au>
2024-05-14 17:52:36 +1000

    Add comment to client and server files

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import socket
 import ssl
 import sys
 from os.path import exists as file_exists
-import traceback
 
 from util.uitl import *
 from util.prorocol import *
@@ -16,15 +15,18 @@ def main():
     print_and_exit("Usage: {} <server name> <port>".format(sys.argv[0]))
   PORT = validate_port(sys.argv[2])
   server_address = sys.argv[1]
-
+  # Retrieve username and password and save to file
   username, password, auth_type = get_user_info()
   data = json.dumps({'username': username, 'password':password, 'auth_type':auth_type})
+  # Create client socket
   client_socket = ssl_client(server_address, PORT)
 
   try:
     # Send data to the server
     client_socket.sendall(data.encode())
+    # Receive login status
     res = int(client_socket.recv(1024).decode())
+    # Loop till successful login
     while res < 0:
       message = 'Login failed' if res == -1 else 'Username already exists'
       print(message)
@@ -33,7 +35,7 @@ def main():
       client_socket.sendall(data.encode())
       res = int(client_socket.recv(1024).decode())
     print('Login successful')
-
+    # Create RSA key pair
     pubkey_file = f"keys/{username}_pubkey.pem"
     privkey_file = f"keys/{username}_privkey.pem"
     if not file_exists(pubkey_file) or not file_exists(privkey_file):
@@ -42,7 +44,7 @@ def main():
     with open(pubkey_file, 'rb') as key_file:
       public_key_content = key_file.read()
     send_public_key(pubkey_file, client_socket)
-
+    # Retrieve number of messages
     num_msg = int(client_socket.recv(1024).decode())
     print(f"You have {num_msg} unread message(s)")
 
@@ -55,7 +57,7 @@ def main():
 
       if command == "READ":
         data['command'] =  command
-        client_socket.sendall(json.dumps(data).encode())
+        client_socket.sendall(json.dumps(data).encode()) # Send coomand to server
         response = client_socket.recv(2048).decode()
         if response == "READ ERROR":
           print(">>> You have no messages\n")
@@ -68,22 +70,27 @@ def main():
             print(f">>> {response_json['recipient']} read your message: {decrypted_message}\n")
           else:
             print(f">>> {response_json['sender']}\n>>> {decrypted_message}\n")
+            
       elif command == "COMPOSE":
         recipient = get_username2("Enter recipient's name: ")
         message = ascii_input("Enter message to send: ")
         if not recipient in pkeys:
+          # Request user's public key from server
           data = json.dumps({'command': command, 'recipient':recipient})
           client_socket.sendall(data.encode())
           response = client_socket.recv(1024).decode()
           if response == 'None':
             print(f'>>> User {recipient} does not exist. Message was not sent.')
           else:
+            # Save public key
             pkeys[recipient] = response
+            # Create message hash
             message_hash = hash_password(message)
+            # Encrypt message
             encrypted_message = encrypt_with_public_key2(message, pkeys[recipient])
+            # Encode request to json and send
             data = json.dumps({'command': command, 'recipient':recipient, 'message':encrypted_message, 'hash': message_hash})
             client_socket.sendall(data.encode())
-            #response = client_socket.recv(1024).decode()
             response = client_socket.recv(2048).decode()
             if response == "MESSAGE SENT":
               print(">>> Message sent successfully\n")
@@ -94,12 +101,14 @@ def main():
         
       elif command == "EXIT":
         data['command'] = 'EXIT'
+        # Send command and exit
         client_socket.sendall(json.dumps(data).encode())
         print(">>> Disconnected from server")
         connected = False
       else:
         print(">>> Invalid command")
 
+    client_socket.close()
   except Exception as e:
      print(f"Client error: {e}")
      sys.exit(1)
--- server.py
+++ server.py
@@ -4,67 +4,81 @@ import ssl
 from database import *
 from util.prorocol import *
 
+# Load messages 
 messages = load_messages('data/messages.json')
+# Load public keys and users
 public_keys = load_public_keys('data/keys.json')
 users_dict = load_users('data/users.json')
 
 def main():
+  """Creates an SSL socket and listens for connection requests and responds to user requests."""
   if len(sys.argv) != 2:
     print_and_exit("Usage: {} <port>".format(sys.argv[0]))
   PORT = validate_port(sys.argv[1])
-  sock= socket_server(PORT)
+  sock= socket_server(PORT) # Create TCP socket
   
   while True:
-    server_socket = ssl_socket(sock)
+    server_socket = ssl_socket(sock) # Create SSL socket
     result, user = authenticate(server_socket)
-
+    # Loop until vailid authentication
     while result < 0:
       server_socket.sendall(str(result).encode())
       result, user = authenticate(server_socket)
     username = user['username']
     recipient = ''
     try:
+      # Send login status to client
       server_socket.sendall(str(result).encode())
-      
+      # Receive user's public key
       data = server_socket.recv(1024)
       # Deserialize JSON string to Python dictionary
       json_data = json.loads(data.decode())
+      # Store public key
       public_keys[username] = json_data['pkey']
       save_public_key(username, json_data['pkey'], 'data/keys.json')
 
+      # Check and send mumber of messages
       if not username in messages:
         messages[username] = []
       num_msg = len(messages[username])
       server_socket.sendall(str(num_msg).encode())
       connected = True
+
       while connected:
         # Receive data from the client
         data = server_socket.recv(1024)
         # Deserialize JSON string to Python dictionary
         json_data = json.loads(data.decode())
         command = json_data['command']
+
+        # READ command
         if command == 'READ':
           if len(messages[username]) > 0:
-            current_message = messages[username].pop(0)
-            responese = json.dumps(current_message)
+            current_message = messages[username].pop(0) # Get oldest message
+            responese = json.dumps(current_message) # Encode to json
             server_socket.sendall(responese.encode())
+            # Store read receipt
             if current_message['type'] == 'unread':
               current_message['type'] = 'read'
               messages[username].append(current_message)
           else:
             responese = 'READ ERROR'
             server_socket.sendall(responese.encode())
+
+        # COMPOSE command
         if command == 'COMPOSE':
           recipient = json_data['recipient']
+          # If key request
           if not 'message' in json_data:
             if not recipient in public_keys:
               server_socket.sendall('None'.encode())
+            # Send recipients public key
             elif recipient in public_keys:
               server_socket.sendall(public_keys[recipient].encode())
               data = server_socket.recv(2048)
               # Deserialize JSON string to Python dictionary
               json_data = json.loads(data.decode())
-            
+              # Construct and store message
               message = {'sender': username, 'message': json_data['message'], 'recipient': recipient, 'hash': \
                         json_data['hash'], 'type': 'unread'}
               if recipient in messages:
@@ -77,6 +91,8 @@ def main():
             else:
               responese = 'MESSAGE SENT'
               server_socket.sendall(responese.encode())
+
+        # EXIT command
         if command == 'EXIT':
           save_messages(messages, 'data/messages.json')
           connected =False
